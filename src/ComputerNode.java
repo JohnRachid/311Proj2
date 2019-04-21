8e974cf3312bee81d8e9e21a684867766fadce93
John Rachid <Jrachid@iastate.edu>
2019-04-21 17:03:30 -0500

    finished ComputerNode class except for getOutNeighbors

--- src/ComputerNode.java
+++ src/ComputerNode.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class ComputerNode {
+    private int id;
+    private int timestamp;
+    int getID(){
+        return id;
+    }
+    int getTimestamp(){
+        return timestamp;
+    }
+    List<ComputerNode> getOutNeighbors(int c){
+        return null; //TODO finish getOutNeighbors
+    }
 }
