18477c6e5daa8781d749497d24cbb6fad4124de1
John Rachid <jrachid@iastate.edu>
2019-04-21 18:05:43 -0500

    Changed some typos and added some code to addCommunication

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,25 +1,32 @@
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CommunicationsMonitor {
     private HashMap<Integer, List<ComputerNode>> G;
-
+    public ArrayList<ComputerNode> computerNodeList;
     public CommunicationsMonitor(){
         G = new HashMap<Integer, List<ComputerNode>>;
+        ArrayList computerNodeList = new ArrayList<ComputerNode>();
     }
 
     public void createGraph(){
 
     }
 
-    public void addCommunication(){
-    
+    public void addCommunication(int c1, int c2, int timestamp){
+        ComputerNode a = new ComputerNode(c1,timestamp);
+        ComputerNode b = new ComputerNode(c2,timestamp);
+        computerNodeList.add(a);
+        computerNodeList.add(b);
+
     }
 
-    public List<ComputerNode> queryInfection(int c1, intc2, int x, int y){
+    public List<ComputerNode> queryInfection(int c1, int c2, int x, int y){
 
     }
 
-    public HashMap<Integer, List<ComputerNode>> get ComputerMapping(){
+    public HashMap<Integer, List<ComputerNode>> getComputerMapping(){
         return G;
     }
 
--- src/ComputerNode.java
+++ src/ComputerNode.java
@@ -3,6 +3,11 @@ import java.util.List;
 public class ComputerNode {
     private int id;
     private int timestamp;
+    ComputerNode(int newID, int newTimestamp){
+        id = newID;
+        timestamp = newTimestamp;
+    }
+
     int getID(){
         return id;
     }
