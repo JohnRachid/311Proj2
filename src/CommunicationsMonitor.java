25f10745c5c7244cbcc374435733a17612d0c2b3
briang0 <brianguidarini@gmail.com>
2019-04-21 17:36:43 -0500

    Setup communicationsmonitor

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,2 +1,29 @@
+import java.util.HashMap;
+
 public class CommunicationsMonitor {
+    private HashMap<Integer, List<ComputerNode>> G;
+
+    public CommunicationsMonitor(){
+        G = new HashMap<Integer, List<ComputerNode>>;
+    }
+
+    public void createGraph(){
+
+    }
+
+    public void addCommunication(){
+    
+    }
+
+    public List<ComputerNode> queryInfection(int c1, intc2, int x, int y){
+
+    }
+
+    public HashMap<Integer, List<ComputerNode>> get ComputerMapping(){
+        return G;
+    }
+
+    public List<ComputerNode> getComputerMapping(int c){
+
+    }
 }
